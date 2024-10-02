c6a3e971561c4ba5c119ada7cfa6f66167cbf0a1
Shvec <Shvec@10KMMRDOTA2>
2024-10-02 21:18:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+	float a,c;
+	int b;
+	cin >> a >> b;
+	for (int i = 0; i <= b; i++) {
+		if (i % 2 != 0) {
+			c = a * b;
+			a = c;
+		}
+		else if (i % 2 == 0) {
+			c = a * (-b);
+			a = c;
+		}
+	cout << c;
+	}
+}
